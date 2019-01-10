ceff03a3050f95db1db7c79b546492c84fd45c49
Graham Goldstein <graham.goldstein@jhc.co.uk>
2019-01-10 08:59:28 +0000

    Add CORS

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_restful import Api
+from flask_cors import CORS
 from waitress import serve
 from resources import add_resources
 
@@ -7,5 +8,7 @@ app = Flask(__name__)
 api = Api(app)
 
 add_resources(api)
+
+CORS(app)
 
 serve(app, host='0.0.0.0', port='5000')
